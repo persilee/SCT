49337187c93fd8b1c1b90bf69d2812dbad09d555
lishaoying <li_shy@hisuntech.com>
2019-01-14 17:18:41 +0800

    🍏  add dynamic digital

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('.value1').numberRock({
+        speed: 5,
+        count: 37
+    })
+    $('.value2').numberRock({
+        speed: 5,
+        count: 25
+    })
+    $('.value3').numberRock({
+        speed: 1,
+        count: 4
+    })
+})
